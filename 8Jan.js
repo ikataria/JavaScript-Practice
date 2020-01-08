59295839c7d101392476e4b773bf1e3b43e2114a
mohit kataria <mohit@shippigo.com>
2020-01-08 19:53:35 +0530

    first commit

---
+++ 8Jan.js
@@ -0,0 +1,42 @@
+// Question 1. Write a function to find the maximum and minimum number form an array without using inbuilt functions.
+
+// let arr = [10, 4, 50, 510, 21, 621, 1, 4851, 54, 445, 548, 844]
+
+// function find(search) {
+//     let temp = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (search.toLowerCase() === 'max') {
+//             console.log('index::::-', i)
+//             console.log('value', arr[i + 1])
+//             if (arr[i] > arr[i + 1]) {
+//                 console.log('here=-=-==-=-===-==-=-=>', arr[i])
+//                 temp
+
+//             }else{
+
+//             }
+
+//         } else {
+//             console.log('nothing')
+//         }
+
+//     }
+
+// }
+// console.log('^^^^^^^^^^^^>', find('max'))
+
+// Question 2. Write a JavaScript function to round a number to a given decimal places.
+
+// Question 3. Get the product of all elements in an array by using higher order functions.
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// let temp = 1;
+
+// for (let value of arr) {
+//     temp *= value
+// }
+// console.log(temp)
+
+// Question 4. Write a program to print elements of an array with and without using higher order functions.
